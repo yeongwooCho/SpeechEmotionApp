4ad5604a33dc759cba9632ecb371cd8f68c985b8
yeongwoo <jyy0223@naver.com>
2021-06-28 16:48:39 +0900

    Initial Commit

---
+++ SpeechEmotionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeechEmotionApp
+//
+//  Created by 조영우 on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
