4d05d657abe502ccb29a4137ed63cefa3cb69a60
yeongwoo <jyy0223@naver.com>
2021-07-25 21:36:01 +0900

    Fix typo

--- SpeechEmotionApp/Extension/UITextField.swift
+++ SpeechEmotionApp/Extension/UITextField.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UITextField {
     func underlined(viewSize: CGFloat, color: UIColor){
         let border = CALayer()
-        let width = CGFloat(2)
+        let width = CGFloat(1)
         border.borderColor = color.cgColor
         border.frame = CGRect(x: 0, y: self.frame.size.height + 10, width: viewSize - 48, height: width)
         border.borderWidth = width
